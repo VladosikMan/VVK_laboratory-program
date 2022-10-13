be99c5b5642c5020249b7c360cd451ac352eae24
Vladislav Vedernikov <vladved2424@gmail.com>
2022-10-13 16:05:22 +0300

    add entity Client

--- Client/ClientLab/Extensions/EnumExtensions.cs
+++ Client/ClientLab/Extensions/EnumExtensions.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 
-namespace ModelsLibrary.Extensions
+namespace ClientLab.Extensions
 {
     public static class EnumExtensions
     {
