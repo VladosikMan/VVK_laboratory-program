c9168de2f6b952ef70aa7783d0751d035ccf5305
VladosikMan <vladgadved18@mail.ru>
2022-10-19 00:12:42 +0300

    fix merge

--- СonstructorVVK/ViewModel/MainViewModel.cs
+++ СonstructorVVK/ViewModel/MainViewModel.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using ModelsLibrary;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Text;
+using СonstructorVVK.recources;
 
 namespace СonstructorVVK.ViewModel
 {
@@ -17,6 +21,35 @@ namespace СonstructorVVK.ViewModel
             if (codeBehind == null) throw new ArgumentNullException(nameof(codeBehind));
 
             _MainCodeBehind = codeBehind;
+        }
+        //привязанные данные
+        public ObservableCollection<Lab> labs = new ObservableCollection<Lab>();
+
+
+
+        //сохранение json лабы в папку
+        public void saveLab(Lab lab)
+        {
+            try
+            {
+                File.WriteAllText(StringRecources.PATH_TO_SAVE_FILE + lab.Name + ".json", lab.Serialize());
+            }
+            catch
+            {
+
+            }
+        }
+        //получить список сохранённых лаб
+        public void loadAllLab()
+        {
+            string[] filePaths = Directory.GetFiles(StringRecources.PATH_TO_SAVE_FILE, "*.json");
+            foreach (string x in filePaths)
+                labs.Add(new Lab(File.ReadAllText(x)));
+        }
+        public void createLab()
+        {
+            var lab = new Lab("TextNameLab" + labs.Count, 80, labs.Count);
+            labs.Add(lab);
         }
     }
 }
