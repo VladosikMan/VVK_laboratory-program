3e92df76dc8be5692cf011c2ebd103227dc4196f
DaniilTimin <mr.timin2012@yandex.ru>
2022-10-23 18:23:14 +0300

    Add schema drawer folder

---
+++ DiagramDesignerMVVM/DemoApp.Persistence.RavenDB/DatabaseAccessService.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Raven.Client.Embedded;
+using Raven.Client;
+using DemoApp.Persistence.Common;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace DemoApp.Persistence.RavenDB
+{
+	/// <summary>
+	/// I decided to use RavenDB instead of SQL, to save people having to have SQL Server, and also
+	/// it just takes less time to do with Raven. This is ALL the CRUD code. Simple no?
+	/// 
+	/// Thing is the IDatabaseAccessService and the items it persists could easily be applied to helper methods that
+	/// use StoredProcedures or ADO code, the data being stored would be exactly the same. You would just need to store
+	/// the individual property values in tables rather than store objects.
+	/// </summary>
+	public class DatabaseAccessService : IDatabaseAccessService
+	{
+		public bool Compare(Diagram diagram)
+		{
+			return diagram.Equals(JsonConvert.DeserializeObject<Diagram>(File.ReadAllText("123.json")));
+		}
+
+		public Diagram DiagramToSave = new Diagram();
+
+		public void DeleteConnection(int connectionId)
+		{
+			
+		}
+
+		public void DeletePersistDesignerItem(int persistDesignerId)
+		{
+			
+		}
+
+		public void DeleteSettingDesignerItem(int settingsDesignerItemId)
+		{
+			
+		}
+
+		public int SaveDiagram(DiagramItem diagram)
+		{
+			DiagramToSave.DiagramItem = diagram;
+			JsonSerializer serializer = new JsonSerializer();
+			serializer.Converters.Add(new JavaScriptDateTimeConverter());
+			serializer.NullValueHandling = NullValueHandling.Ignore;
+
+			using (StreamWriter sw = new StreamWriter("123.json"))
+			using (JsonWriter writer = new JsonTextWriter(sw))
+			{
+				serializer.Serialize(writer, DiagramToSave);
+			}
+			
+			return diagram.Id;
+		}
+
+		public int SavePersistDesignerItem(PersistDesignerItem persistDesignerItemToSave)
+		{
+			DiagramToSave.PersistDesignerItem.Add(persistDesignerItemToSave);
+
+			return persistDesignerItemToSave.Id;
+		}
+
+		public int SaveSettingDesignerItem(SettingsDesignerItem settingsDesignerItemToSave)
+		{
+			DiagramToSave.SettingsDesignerItem.Add(settingsDesignerItemToSave);
+
+			return settingsDesignerItemToSave.Id;
+		}
+
+		public int SaveConnection(Connection connectionToSave)
+		{
+			DiagramToSave.Connections.Add(connectionToSave);
+
+			return connectionToSave.Id;
+		}
+
+		public int SaveGroupingDesignerItem(GroupDesignerItem groupDesignerItem)
+		{
+			DiagramToSave.GroupDesignerItem.Add(groupDesignerItem);
+
+			return groupDesignerItem.Id;
+		}
+
+		public IEnumerable<DiagramItem> FetchAllDiagram()
+		{
+			var list = new List<DiagramItem>();
+			return list;
+		}
+
+		public DiagramItem FetchDiagram(int diagramId)
+		{
+			DiagramToSave = new Diagram();
+			DiagramToSave.DiagramItem.ResetCount();
+			DiagramToSave = JsonConvert.DeserializeObject<Diagram>(File.ReadAllText("123.json"));
+			return DiagramToSave.DiagramItem;
+		}
+
+		public PersistDesignerItem FetchPersistDesignerItem(int settingsDesignerItemId)
+		{
+			return DiagramToSave.PersistDesignerItem.Find(x => x.Id.Equals(settingsDesignerItemId));
+		}
+
+		public SettingsDesignerItem FetchSettingsDesignerItem(int settingsDesignerItemId)
+		{
+			return DiagramToSave.SettingsDesignerItem.Find(x => x.Id.Equals(settingsDesignerItemId));
+		}
+
+		public Connection FetchConnection(int connectionId)
+		{
+			return DiagramToSave.Connections.Find(x => x.Id.Equals(connectionId));
+		}
+
+		public GroupDesignerItem FetchGroupingDesignerItem(int itemId)
+		{
+			return DiagramToSave.GroupDesignerItem.Find(x => x.Id.Equals(itemId));
+		}
+	}
+}
---
+++ DiagramDesignerMVVM/DemoApp/Drawer.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DiagramDesigner.Helpers;
+using DiagramDesigner;
+
+namespace DemoApp
+{
+	/// <summary>
+	/// Interaction logic for Drawer.xaml
+	/// </summary>
+	public partial class Drawer : UserControl
+	{
+		private Window1ViewModel window1ViewModel;
+
+		public Drawer()
+		{
+            InitializeComponent();
+
+			window1ViewModel = new Window1ViewModel();
+			this.DataContext = window1ViewModel;
+			this.Loaded += new RoutedEventHandler(Window1_Loaded);
+		}
+
+        /// <summary>
+        /// This shows you how you can create diagram items in code, which means you can 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void Window1_Loaded(object sender, RoutedEventArgs e)
+        {
+            SettingsDesignerItemViewModel item1 = new SettingsDesignerItemViewModel();
+            item1.Parent = window1ViewModel.DiagramViewModel;
+            item1.Left = 100;
+            item1.Top = 100;
+            window1ViewModel.DiagramViewModel.Items.Add(item1);
+
+            PersistDesignerItemViewModel item2 = new PersistDesignerItemViewModel();
+            item2.Parent = window1ViewModel.DiagramViewModel;
+            item2.Left = 300;
+            item2.Top = 300;
+            window1ViewModel.DiagramViewModel.Items.Add(item2);
+
+            ConnectorViewModel con1 = new ConnectorViewModel(item1.RightConnector, item2.TopConnector);
+            con1.Parent = window1ViewModel.DiagramViewModel;
+            window1ViewModel.DiagramViewModel.Items.Add(con1);
+        }
+    }
+}
