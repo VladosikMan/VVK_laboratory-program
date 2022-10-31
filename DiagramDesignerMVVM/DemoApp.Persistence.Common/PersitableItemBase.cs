84a1b615cdbd53ba79250a70c874313db96ff460
VladosikMan <vladgadved18@mail.ru>
2022-10-31 23:41:03 +0300

    Add schema drawer folder

---
+++ DiagramDesignerMVVM/DemoApp.Persistence.Common/PersitableItemBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DemoApp.Persistence.Common
+{
+    public abstract class PersistableItemBase
+    {
+	    public static int count = 0;
+
+        public PersistableItemBase()
+        {
+	        
+        }
+
+        public PersistableItemBase(int id)
+        {
+            this.Id = id;
+            count++;
+        }
+
+        public int Id { get; set; }
+
+        public void ResetCount()
+        {
+	        count = 0;
+        }
+    }
+}
